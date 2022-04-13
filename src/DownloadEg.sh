2ae91819db220594612848588bd8a65c268a6946
RoseHuRan <116010078@link.cuhk.edu.cn>
2022-04-12 18:18:37 -0700

    add example

---
+++ src/DownloadEg.sh
@@ -0,0 +1,9 @@
+# download example fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR850/ERR850376/ERR850376_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR850/ERR850376/ERR850376_2.fastq.gz
+
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR852/ERR852106/ERR852106_1.fastq.gz
+wget ftp://ftp.sra.ebi.ac.uk/vol1/fastq/ERR852/ERR852106/ERR852106_2.fastq.gz
+
+
